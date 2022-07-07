67c0b1b597c7edc1db14ebe2962d76b8ab001af8
Brian Shaginaw <bshaginaw120@gmail.com>
2022-07-07 02:09:52 -0400

    Accept filename to run

--- src/bin/run.rs
+++ src/bin/run.rs
@@ -1,5 +1,14 @@
+use std::env;
+
 fn main() -> Result<(), String> {
-    let piet = piet_tools::load("examples/hw6_big.png", 5)?;
+    let args = env::args().collect::<Vec<_>>();
+    let filename = match args.as_slice() {
+        [_, filename, ..] => filename,
+        _ => { return Err("usage: pietc filename".to_string()); },
+    };
+
+    let piet = piet_tools::load(filename, 5)?;
     piet.execute().run();
+    println!();
     Ok(())
 }

52b8954fe526055a30ef9a6c2ef3331ed2ef4897
Brian Shaginaw <bshaginaw120@gmail.com>
2022-07-07 11:39:09 -0400

    Move interpreter to pieti

--- src/bin/pieti.rs
+++ src/bin/pieti.rs
@@ -4,7 +4,7 @@ fn main() -> Result<(), String> {
     let args = env::args().collect::<Vec<_>>();
     let filename = match args.as_slice() {
         [_, filename, ..] => filename,
-        _ => { return Err("usage: pietc filename".to_string()); },
+        _ => { return Err("usage: pieti filename".to_string()); },
     };
 
     let piet = piet_tools::load(filename, 5)?;

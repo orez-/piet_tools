02ffc379b38cfab5bffc351b557eaf9a3fff10c7
Brian Shaginaw <bshaginaw120@gmail.com>
2022-07-08 15:13:25 -0400

    Begin an optimizer
    Stopped immediately by Piet's lenient rules about ignoring commands
    that grab more arguments than exist

--- src/asm.rs
+++ src/asm.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Clone)]
 enum AsmCommand {
     Push(BigInt),
     Pop,
@@ -286,8 +286,14 @@ struct ParseContext {
 
 fn parse(lines: &[String]) -> Result<PietAsm, ParseError> {
     let lines = preprocess(lines)?;
+    let asm = to_bytecode(lines)?;
+    let asm = optimize(asm);
+    Ok(asm)
+}
+
+fn to_bytecode(ast: Vec<Line>) -> Result<PietAsm, ParseError> {
     let mut context = ParseContext::default();
-    for line in lines {
+    for line in ast {
         let lineno = line.lineno;
         parse_line(line, &mut context)
             .map_err(|e| e.at(lineno))?;
@@ -388,4 +394,72 @@ pub fn load(filename: &str) -> Result<PietAsm, String> {
     let lines: Result<Vec<_>, _> = reader.lines().collect();
     let lines = lines.map_err(|e| e.to_string())?;
     parse(&lines).map_err(|e| format!("{e:?}"))
+}
+
+fn optimize(mut asm: PietAsm) -> PietAsm {
+    use AsmCommand::*;
+
+    fn push(val: i32) -> AsmCommand {
+        Push(val.into())
+    }
+
+    // TODO: [dyad, POP] => [POP, POP]
+    // let constant_patterns: [(Vec<AsmCommand>, Vec<AsmCommand>); _] = [
+    let constant_patterns: [(Vec<AsmCommand>, Vec<AsmCommand>); 0] = [
+        // XXX: these are all predicated on there being something on the stack!
+        // (vec![push(1), Multiply], Vec::new()),
+        // (vec![push(1), Divide], Vec::new()),
+        // // push(0) needs to get replaced later anyway,
+        // // so if we've got a pop handy, instead
+        // (vec![Pop, push(0)], vec![push(1), Mod]),
+    ];
+    'progress: while {
+        // [PUSH T, PUSH T] => [PUSH T, DUPLICATE]
+        if let Some(idx) = asm.cmds
+            .windows(2)
+            .rposition(|w| matches!(w[0], Push(_)) && w[0] == w[1])
+        {
+            asm.cmds[idx + 1] = Duplicate;
+            continue 'progress
+        }
+
+        // Run through all the constant patterns
+        for (needle, replace_with) in &constant_patterns {
+            let len = needle.len();
+            if let Some(idx) = asm.cmds
+                .windows(len)
+                .position(|w| w == needle.as_slice())
+            {
+                asm.cmds.splice(idx..idx + len, replace_with.iter().cloned());
+                continue 'progress;
+            }
+        }
+        false
+    } {}
+
+    asm
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::asm::*;
+    use crate::asm::AsmCommand::*;
+
+    fn push(val: i32) -> AsmCommand {
+        Push(val.into())
+    }
+
+    #[test]
+    fn test_dup_pushes() {
+        let asm = PietAsm { cmds: vec![push(5), push(2), push(2), push(2), push(8), push(8)] };
+        let PietAsm { cmds, .. } = optimize(asm);
+        assert_eq!(cmds, vec![push(5), push(2), Duplicate, Duplicate, push(8), Duplicate]);
+    }
+
+    #[test]
+    fn test_stack_bump() {
+        let asm = PietAsm { cmds: vec![push(1), Multiply] };
+        let PietAsm { cmds, .. } = optimize(asm);
+        assert_eq!(cmds, vec![push(1), Multiply]);
+    }
 }

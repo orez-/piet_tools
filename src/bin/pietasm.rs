68eae6a73b3231be7abbc3f18979cbc47939b520
Brian Shaginaw <bshaginaw120@gmail.com>
2022-07-10 11:31:00 -0400

    Add codel_size to cli's

--- src/bin/pietasm.rs
+++ src/bin/pietasm.rs
@@ -2,14 +2,19 @@ use std::env;
 
 fn main() -> Result<(), String> {
     let args = env::args().collect::<Vec<_>>();
-    let filename = match args.as_slice() {
-        [_, filename, ..] => filename,
-        _ => { return Err("usage: pietasm filename".to_string()); },
+    let (filename, codel_size) = match args.as_slice() {
+        [_, f, c, ..] => (f, c),
+        _ => { return Err("usage: pietasm filename codel-size".to_string()); },
     };
+    let codel_size = codel_size.parse()
+        .map_err(|_| "codel-size must be an integer".to_string())?;
+    if codel_size == 0 {
+        return Err("codel-size must be non-zero".to_string())
+    }
 
     let piet = piet_tools::asm::load(filename)?;
     let out_filename = format!("{filename}.png");
-    piet_tools::save(piet, &out_filename, 10)
+    piet_tools::save(piet, &out_filename, codel_size)
         .map_err(|e| e.to_string())?;
     println!("File saved to {out_filename}");
     Ok(())
--- src/bin/pieti.rs
+++ src/bin/pieti.rs
@@ -2,12 +2,17 @@ use std::env;
 
 fn main() -> Result<(), String> {
     let args = env::args().collect::<Vec<_>>();
-    let filename = match args.as_slice() {
-        [_, filename, ..] => filename,
-        _ => { return Err("usage: pieti filename".to_string()); },
+    let (filename, codel_size) = match args.as_slice() {
+        [_, f, c] => (f, c),
+        _ => { return Err("usage: pieti filename codel-size".to_string()); },
     };
+    let codel_size = codel_size.parse()
+        .map_err(|_| "codel-size must be an integer".to_string())?;
+    if codel_size == 0 {
+        return Err("codel-size must be non-zero".to_string())
+    }
 
-    let piet = piet_tools::load(filename, 10)?;
+    let piet = piet_tools::load(filename, codel_size)?;
     piet.execute().run();
     println!();
     Ok(())
